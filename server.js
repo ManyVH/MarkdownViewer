f7e37303c13b20aa4e59223e1381cb8194c668e9
José Manuel Varela Hernández <josemanuelvarela36@gmail.com>
2024-07-04 20:33:04 -0600

    MarkDown-Fase 1

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/view', function (req, res) {
+    res.render('pad')
+    
+})
+
+const port = 3000
+
+app.listen(port)
